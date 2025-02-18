4f58e977e4d1a96cd5f89eb2577aa6212535be84
Nguyen Van Thai <nguyenvanthai0212@gmail.com>
2025-02-19 02:47:31 +0700

    Fix commands

--- sf_examples/vizdoom/experiments/doom_basic.py
+++ sf_examples/vizdoom/experiments/doom_basic.py
@@ -29,17 +29,17 @@ _experiment_vision_sound_mel = Experiment(
 )
 _experiment_no_vision_sound_mel = Experiment(
     "doom_basic_no_vision_mel",
-    "python -m sf_examples.vizdoom.train_vizdoom --env=doom_basic --train_for_env_steps=2000000 --algo=APPO --env_frameskip=4 --use_rnn=True --wide_aspect_ratio=False --num_workers=20 --num_envs_per_worker=16 --decorrelate_envs_on_one_worker=False --use_sound --audio_encoder=mel --encoder_conv_architecture --train_dir=train_dir/doom_basic_no_vision_mel",
+    "python -m sf_examples.vizdoom.train_vizdoom --env=doom_basic --train_for_env_steps=2000000 --algo=APPO --env_frameskip=4 --use_rnn=True --wide_aspect_ratio=False --num_workers=20 --num_envs_per_worker=16 --decorrelate_envs_on_one_worker=False --use_sound --audio_encoder=mel --encoder_conv_architecture=none --train_dir=train_dir/doom_basic_no_vision_mel",
     _params.generate_params(randomize=False),
 )
 _experiment_no_vision_sound_fft = Experiment(
     "doom_basic_no_vision_fft",
-    "python -m sf_examples.vizdoom.train_vizdoom --env=doom_basic --train_for_env_steps=2000000 --algo=APPO --env_frameskip=4 --use_rnn=True --wide_aspect_ratio=False --num_workers=20 --num_envs_per_worker=16 --decorrelate_envs_on_one_worker=False --use_sound --audio_encoder=fft --encoder_conv_architecture --train_dir=train_dir/doom_basic_no_vision_fft",
+    "python -m sf_examples.vizdoom.train_vizdoom --env=doom_basic --train_for_env_steps=2000000 --algo=APPO --env_frameskip=4 --use_rnn=True --wide_aspect_ratio=False --num_workers=20 --num_envs_per_worker=16 --decorrelate_envs_on_one_worker=False --use_sound --audio_encoder=fft --encoder_conv_architecture=none --train_dir=train_dir/doom_basic_no_vision_fft",
     _params.generate_params(randomize=False),
 )
 _experiment_no_vision_sound_raw = Experiment(
     "doom_basic_no_vision_raw",
-    "python -m sf_examples.vizdoom.train_vizdoom --env=doom_basic --train_for_env_steps=2000000 --algo=APPO --env_frameskip=4 --use_rnn=True --wide_aspect_ratio=False --num_workers=20 --num_envs_per_worker=16 --decorrelate_envs_on_one_worker=False --use_sound --audio_encoder=raw --encoder_conv_architecture --train_dir=train_dir/doom_basic_no_vision_raw",
+    "python -m sf_examples.vizdoom.train_vizdoom --env=doom_basic --train_for_env_steps=2000000 --algo=APPO --env_frameskip=4 --use_rnn=True --wide_aspect_ratio=False --num_workers=20 --num_envs_per_worker=16 --decorrelate_envs_on_one_worker=False --use_sound --audio_encoder=raw --encoder_conv_architecture=none --train_dir=train_dir/doom_basic_no_vision_raw",
     _params.generate_params(randomize=False),
 )
 
--- sf_examples/vizdoom/experiments/doom_battle_battle2_appo.py
+++ sf_examples/vizdoom/experiments/doom_battle_battle2_appo.py
@@ -31,17 +31,17 @@ _experiments = [
     ),
     Experiment(
         "battle_fs4_no_vision_raw",
-        "python -m sf_examples.vizdoom.train_vizdoom --train_for_env_steps=4000000000 --algo=APPO --env_frameskip=4 --use_rnn=True --num_workers=16 --num_envs_per_worker=20 --num_policies=1 --batch_size=2048 --wide_aspect_ratio=False --max_grad_norm=0.0 --train_dir=train_dir/battle_fs4_no_vision_raw --audio_encoder=raw --use_sound",
+        "python -m sf_examples.vizdoom.train_vizdoom --train_for_env_steps=4000000000 --algo=APPO --env_frameskip=4 --use_rnn=True --num_workers=16 --num_envs_per_worker=20 --num_policies=1 --batch_size=2048 --wide_aspect_ratio=False --max_grad_norm=0.0 --train_dir=train_dir/battle_fs4_no_vision_raw --audio_encoder=raw --use_sound --encoder_conv_architecture=none",
         _params.generate_params(randomize=False),
     ),
     Experiment(
         "battle_fs4_no_vision_fft",
-        "python -m sf_examples.vizdoom.train_vizdoom --train_for_env_steps=4000000000 --algo=APPO --env_frameskip=4 --use_rnn=True --num_workers=16 --num_envs_per_worker=20 --num_policies=1 --batch_size=2048 --wide_aspect_ratio=False --max_grad_norm=0.0 --train_dir=train_dir/battle_fs4_no_vision_fft --audio_encoder=fft --use_sound",
+        "python -m sf_examples.vizdoom.train_vizdoom --train_for_env_steps=4000000000 --algo=APPO --env_frameskip=4 --use_rnn=True --num_workers=16 --num_envs_per_worker=20 --num_policies=1 --batch_size=2048 --wide_aspect_ratio=False --max_grad_norm=0.0 --train_dir=train_dir/battle_fs4_no_vision_fft --audio_encoder=fft --use_sound --encoder_conv_architecture=none",
         _params.generate_params(randomize=False),
     ),
 Experiment(
         "battle_fs4_no_vision_mel",
-        "python -m sf_examples.vizdoom.train_vizdoom --train_for_env_steps=4000000000 --algo=APPO --env_frameskip=4 --use_rnn=True --num_workers=16 --num_envs_per_worker=20 --num_policies=1 --batch_size=2048 --wide_aspect_ratio=False --max_grad_norm=0.0 --train_dir=train_dir/battle_fs4_no_vision_mel --audio_encoder=mel --use_sound",
+        "python -m sf_examples.vizdoom.train_vizdoom --train_for_env_steps=4000000000 --algo=APPO --env_frameskip=4 --use_rnn=True --num_workers=16 --num_envs_per_worker=20 --num_policies=1 --batch_size=2048 --wide_aspect_ratio=False --max_grad_norm=0.0 --train_dir=train_dir/battle_fs4_no_vision_mel --audio_encoder=mel --use_sound --encoder_conv_architecture=none",
         _params.generate_params(randomize=False),
     ),
 ]
