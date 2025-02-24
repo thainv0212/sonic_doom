828437a3839f9d5e1d1d292a5616361cd5c94804
icelab <icelab@gmail.com>
2025-02-24 22:46:55 +0900

    Change experiment settings

--- sf_examples/vizdoom/experiments/paper_doom_all_basic_envs.py
+++ sf_examples/vizdoom/experiments/paper_doom_all_basic_envs.py
@@ -2,7 +2,8 @@ from sample_factory.launcher.run_description import Experiment, ParamGrid, RunDe
 
 _params = ParamGrid(
     [
-        ("seed", [0, 1111, 2222, 3333, 4444, 5555, 6666, 7777, 8888, 9999]),
+        # ("seed", [0, 1111, 2222, 3333, 4444, 5555, 6666, 7777, 8888, 9999]),
+        ("seed", [0, 1111]),
         # (
         #     "env",
         #     [
@@ -31,49 +32,49 @@ envs = [
 for env in envs:
     _experiments.append(
         Experiment(
-            "basic_envs_fs4_no_sound",
-            f"python -m sf_examples.vizdoom.train_vizdoom --train_for_env_steps=500000000 --algo=APPO --env_frameskip=4 --use_rnn=True --num_workers=36 --num_envs_per_worker=8 --num_policies=1 --batch_size=2048 --wide_aspect_ratio=False --env={env}",
+            f"{env}_basic_envs_fs4_no_sound",
+            f"python -m sf_examples.vizdoom.train_vizdoom --train_for_env_steps=500000000 --algo=APPO --env_frameskip=4 --use_rnn=True --num_workers=36 --num_envs_per_worker=8 --num_policies=1 --batch_size=2048 --wide_aspect_ratio=False --env={env} --train_dir=train_dir/{env}_no_sound",
             _params.generate_params(randomize=False),
         )
     )
     _experiments.append(
         Experiment(
-            "basic_envs_fs4_vision_raw",
+            f"{env}_basic_envs_fs4_vision_raw",
             f"python -m sf_examples.vizdoom.train_vizdoom --train_for_env_steps=500000000 --algo=APPO --env_frameskip=4 --use_rnn=True --num_workers=36 --num_envs_per_worker=8 --num_policies=1 --batch_size=2048 --wide_aspect_ratio=False  --env={env} --use_sound --audio_encoder=raw --train_dir=train_dir/{env}_vision_raw",
             _params.generate_params(randomize=False),
         )
     )
     _experiments.append(
         Experiment(
-            "basic_envs_fs4_vision_fft",
+            f"{env}_basic_envs_fs4_vision_fft",
             f"python -m sf_examples.vizdoom.train_vizdoom --train_for_env_steps=500000000 --algo=APPO --env_frameskip=4 --use_rnn=True --num_workers=36 --num_envs_per_worker=8 --num_policies=1 --batch_size=2048 --wide_aspect_ratio=False  --env={env} --use_sound --audio_encoder=fft --train_dir=train_dir/{env}_vision_fft",
             _params.generate_params(randomize=False),
         )
     )
     _experiments.append(
         Experiment(
-            "basic_envs_fs4_vision_mel",
+            f"{env}_basic_envs_fs4_vision_mel",
             f"python -m sf_examples.vizdoom.train_vizdoom --train_for_env_steps=500000000 --algo=APPO --env_frameskip=4 --use_rnn=True --num_workers=36 --num_envs_per_worker=8 --num_policies=1 --batch_size=2048 --wide_aspect_ratio=False  --env={env} --use_sound --audio_encoder=mel --train_dir=train_dir/{env}_vision_mel",
             _params.generate_params(randomize=False),
         )
     )
     _experiments.append(
         Experiment(
-            "basic_envs_fs4_no_vision_mel",
+            f"{env}_basic_envs_fs4_no_vision_mel",
             f"python -m sf_examples.vizdoom.train_vizdoom --train_for_env_steps=500000000 --algo=APPO --env_frameskip=4 --use_rnn=True --num_workers=36 --num_envs_per_worker=8 --num_policies=1 --batch_size=2048 --wide_aspect_ratio=False  --env={env} --use_sound --audio_encoder=mel --train_dir=train_dir/{env}_no_vision_mel --encoder_conv_architecture=none",
             _params.generate_params(randomize=False),
         )
     )
     _experiments.append(
         Experiment(
-            "basic_envs_fs4_no_vision_fft",
+            f"{env}_basic_envs_fs4_no_vision_fft",
             f"python -m sf_examples.vizdoom.train_vizdoom --train_for_env_steps=500000000 --algo=APPO --env_frameskip=4 --use_rnn=True --num_workers=36 --num_envs_per_worker=8 --num_policies=1 --batch_size=2048 --wide_aspect_ratio=False  --env={env} --use_sound --audio_encoder=fft --train_dir=train_dir/{env}_no_vision_fft --encoder_conv_architecture=none",
             _params.generate_params(randomize=False),
         )
     )
     _experiments.append(
         Experiment(
-            "basic_envs_fs4_no_vision_fft",
+            f"{env}_basic_envs_fs4_no_vision_fft",
             f"python -m sf_examples.vizdoom.train_vizdoom --train_for_env_steps=500000000 --algo=APPO --env_frameskip=4 --use_rnn=True --num_workers=36 --num_envs_per_worker=8 --num_policies=1 --batch_size=2048 --wide_aspect_ratio=False  --env={env} --use_sound --audio_encoder=raw --train_dir=train_dir/{env}_no_vision_raw --encoder_conv_architecture=none",
             _params.generate_params(randomize=False),
         )
